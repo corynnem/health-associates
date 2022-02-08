5c472170630bdeceef0545c7d7e6c0460f806e90
corynnem <corynnem.webdev@gmail.com>
2022-02-08 11:59:44 -0500

    added component fallback to avoid error in prerendering

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
-import Navigation from './Nav/Navigation'
-import App from './app'
-import styles from '../styles/Home.module.css'
-
-
+import React, { useState } from "react";
+import Navigation from "./Nav/Navigation";
+import App from "./app";
+import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import FallBack from "./Fallback";
 
 export default function Home() {
+  const router = useRouter();
 
   return (
-    <div className={styles.container} style={{ width: '100vw' }}>
-      <Navigation  />
-      <App/>
+    <div className={styles.container} style={{ width: "100vw" }}>
+      {router.isFallback ? (
+        <FallBack />
+      ) : (
+        <div>
+          <Navigation />
+          <App />
+        </div>
+      )}
     </div>
-  )
+  );
 }
