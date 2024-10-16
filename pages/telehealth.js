98460b76fc435e6b1c7e0d8e999c97e56a5e269d
Your Name <you@example.com>
2024-10-16 15:26:29 -0400

    update headshot

--- pages/telehealth.js
+++ pages/telehealth.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Navigation from "../components/Navigation/Navigation";
 import Title from "../components/Header/Title";
-import groff from "../../public/headshots/GROFF.jpeg";
-import dodini from "../../public/headshots/DODINI.jpeg";
+import groff from "../public/headshots/GROFF.jpeg";
+import dodini from "../public/headshots/DODINI.jpeg";
 import Footer from "../components/Footer/Footer";
 
 const Telehealth = () => {
